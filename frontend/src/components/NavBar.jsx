fa51bb6a1b1364cd1221f49ad31e2452dc90b848
rizal <rizalfatoni594@gmail.com>
2026-08-28 20:22:42 +0700

    Done with HomePage - NavBar.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,3 +1,22 @@
+import { Link } from 'react-router';
+import { PlusIcon } from 'lucide-react';
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <header className='bg-base-300 border-b border-base-content/10d'>
+      <div className='mx-auto max-w-6xl p-4'>
+        <div className='flex items-center justify-between'>
+          <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>
+            Journee
+          </h1>
+          <div className='flex items-center gap-4'>
+            <Link to={'/create'} className='btn btn-primary'>
+              <PlusIcon className='size-5' />
+              <span>New Note</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
