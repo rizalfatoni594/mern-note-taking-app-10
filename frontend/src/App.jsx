2b878a83d41e500f811bd1bd699bf361f6832480
rizal <rizalfatoni594@gmail.com>
2026-08-31 18:22:29 +0700

    Fixed the NavBar width issue.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,15 @@ import NoteDetailPage from './pages/NoteDetailPage.jsx';
 
 export default function App() {
   return (
-    <div data-theme='light' className='relative h-full w-full'>
-      <div className='absolute inset-0 -z-10 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#FFF_60%,#00FF9D40_100%)]'>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/create' element={<CreatePage />} />
-          <Route path='/note/:id' element={<NoteDetailPage />} />
-        </Routes>
-      </div>
+    <div
+      data-theme='light'
+      className='min-h-screen bg-primary/5 text-base-content'
+    >
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -23,7 +23,7 @@ export default function NoteCard({ note, setNotes }) {
   return (
     <Link
       to={`/note/${note._id}`}
-      className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D40]'
+      className='card bg-base-100 shadow-md shadow-primary/10 hover:shadow-xl transition-all duration-200 border-t-6 border-solid border-primary'
     >
       <div className='card-body'>
         <h3 className='card-title text-base-content'>{note.title}</h3>
