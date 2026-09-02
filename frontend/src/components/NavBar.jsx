6058e0913c0a083f2d9e40376442db267092e103
rizal <rizalfatoni594@gmail.com>
2026-09-02 21:14:00 +0700

    Added icon and catch phrase.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,7 +7,10 @@ export default function NavBar() {
       <div className='mx-auto max-w-6xl p-4'>
         <div className='flex items-center justify-between'>
           <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>
-            Journie
+            Journie🪶
+            <span className='text-sm block text-base-content/70'>
+              It stays here, free your mind.
+            </span>
           </h1>
           <div className='flex items-center gap-4'>
             <Link to={'/create'} className='btn btn-primary'>
--- frontend/src/components/NotesNotFound.jsx
+++ frontend/src/components/NotesNotFound.jsx
@@ -10,7 +10,7 @@ export default function NotesNotFound() {
       <h3 className='text-2xl font-bold'>No notes yet</h3>
       <p className='text-base-content/70'>
         Ready to organize your thoughts? Create your first note to get started
-        on your journee 🚢😀
+        on your Journie 🚢😀
       </p>
       <Link to={'/create'} className='btn btn-primary'>
         Create Your First Note
